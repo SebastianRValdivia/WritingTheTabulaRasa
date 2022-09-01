6fb8ae589e5658f5d7f6d9e98d04cf7b0e344f43
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2022-09-01 18:41:43 -0300

    Add inputs for username and password in dialog

--- client/src/components/LoginDialog.vue
+++ client/src/components/LoginDialog.vue
@@ -1,14 +1,34 @@
 <template>
   <q-dialog v-model="appStore.isLoginOpen" persistent>
-      <q-card>
-        <q-card-section class="row items-center">
-
-
+      <q-card class="q-px-sm q-pb-md column">
+        <q-card-section class="items-center">
+          <q-input v-model="username" filled class="app-login-input">
+            <template v-slot:prepend>
+              <q-icon name="person" />
+            </template>
+          </q-input>
+          <q-input 
+            class="app-login-input"
+            v-model="password"
+            filled 
+            :type="isPwd ? 'password' : 'text'"
+          >
+            <template v-slot:prepend>
+              <q-icon name="key" />
+            </template>
+            <template v-slot:append>
+              <q-icon
+                :name="isPwd ? 'visibility_off' : 'visibility'"
+                class="cursor-pointer"
+                @click="isPwd = !isPwd"
+              />
+            </template>
+          </q-input>
         </q-card-section>
 
         <q-card-actions align="right">
           <q-btn flat label="Cancel" color="warning" v-close-popup />
-          <q-btn flat label="accept" color="primary" @click="logIn" />
+          <q-btn flat label="Proceed" color="primary" @click="logIn" />
         </q-card-actions>
      </q-card>
   </q-dialog>
@@ -16,6 +36,7 @@
 </template>
 
 <script>
+import { ref } from "vue"
 import { useAppStore } from "src/stores/app-store"
 import { useUserStore } from "src/stores/user-store"
 
@@ -23,9 +44,15 @@ export default {
   setup() {
     const appStore = useAppStore()
     const userStore = useUserStore()
+    const username = ref("")
+    const password = ref("")
+    const isPwd = ref(true)
 
     return {
       appStore,
+      username,
+      password,
+      isPwd,
 
       logIn() {
         userStore.logUser()
@@ -35,7 +62,9 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.app-login-input {
+  padding-bottom: 10px;
+}
 
-    
 </style>

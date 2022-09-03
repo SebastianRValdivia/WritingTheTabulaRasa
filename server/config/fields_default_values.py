c3cc5d7b76449dd55ef287fc61ce522af42f1cc6
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2022-09-03 18:31:08 -0300

    Add migrations for note app and default db values

--- server/config/fields_default_values.py
+++ server/config/fields_default_values.py
@@ -2,10 +2,10 @@ import environ
 import os
 from config.settings import BASE_DIR 
 
+
 # Load and set environment variables
 env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
-CHARFIELD_SHORT = env('CHARFIELD_SHORT')
-CHARFIELD_LONG = env('CHARFIELD_LONG')
-
+CHARFIELD_SHORT = int(env('CHARFIELD_SHORT'))
+CHARFIELD_LONG = int(env('CHARFIELD_LONG'))

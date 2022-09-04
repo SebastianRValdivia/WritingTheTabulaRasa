f5db74f52ed5131886d0ab9eb6199e4f23e90a91
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2022-09-04 19:47:18 -0300

    Add note page

---
+++ client/src/pages/NotesPage.vue
@@ -0,0 +1,31 @@
+<template>
+ <div>
+  <h1>Notes</h1>
+  <ul>
+    <li v-for="note in noteStore.notes" :key="note.id">
+      {{ note.title }}
+    </li>
+  </ul>
+
+ </div>
+</template>
+
+<script>
+import { useNoteStore } from "src/stores/note-store"
+
+export default {
+  setup() {
+    const noteStore = useNoteStore()
+    noteStore.retrieveNotes()
+
+    return {
+      noteStore,
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>

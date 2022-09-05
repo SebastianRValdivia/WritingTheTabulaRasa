be53890dc28be3f930fe19fd59875d8a584523a6
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2022-09-05 15:07:29 -0300

    Add note page to router

---
+++ client/src/pages/NotePage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{ props.id }}</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    id: Number
+  },
+  setup(props) {
+
+    return {
+      props
+    }
+
+    
+  }
+}
+
+</script>
--- client/src/router/routes.js
+++ client/src/router/routes.js
@@ -5,7 +5,15 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: "", component: () => import('pages/IndexPage.vue') },
-      { path: "/notes/", component: () => import("src/pages/NoteListPage.vue") }
+      { 
+        path: "/notes/",
+        component: () => import("src/pages/NoteListPage.vue"),
+      },
+      {
+        path: "/notes/:id",
+        component: () => import("src/pages/NotePage.vue"),
+        props: true
+      }
     ]
   },
 

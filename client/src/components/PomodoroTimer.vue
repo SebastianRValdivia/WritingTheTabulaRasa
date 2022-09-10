ecf8c9b63717e8051adcdb0adb6ba3aec7a7b338
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2022-09-10 20:09:10 -0300

    Add basic timer and tool store

---
+++ client/src/components/PomodoroTimer.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-btn round color="red" icon="alarm" @click="launch"/>
+</template>
+
+<script>
+import { ref } from "vue"
+import { useToolStore } from "src/stores/tool-store"
+
+export default {
+  setup() {
+    const workTime = ref(1)
+    const restTime = ref(5)
+    const status = ref(false)
+    const toolStore = useToolStore()
+
+
+    return {
+      workTime,
+      restTime,
+      status,
+
+      launch() {
+        toolStore.runTimer(workTime.value)
+      }
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- client/src/pages/ToolsPage.vue
+++ client/src/pages/ToolsPage.vue
@@ -1,13 +1,21 @@
 <template>
   <h1>Tools</h1>
+  <div class="tool-box">
+    <PomodoroTimer />
+  </div>
 </template>
 
 <script>
-export default {
+import PomodoroTimer from 'src/components/PomodoroTimer.vue';
 
+export default {
+    components: { PomodoroTimer }
 }
 </script>
 
 <style>
+.tool-box {
+  border-bottom: 1px solid black;
+}
 
 </style>

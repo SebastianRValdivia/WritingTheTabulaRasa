700f4e4a862a8211fbac6a7eb6482b93204f8631
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2022-09-12 05:05:43 -0300

    Add timer settings

--- client/src/stores/tool-store.js
+++ client/src/stores/tool-store.js
@@ -4,9 +4,11 @@ import { defineStore } from "pinia";
 export const useToolStore = defineStore("tool", {
   state: () => ({
     timer: null,
-    timerStatus: false, // True = running, false = stop
+    timerStatus: false, // True = running, false = stop TODO: Use timerTime 0 instead
     timerType: false, // True = working, false = resting
-    timerTime: 3, // Minutes to run
+    timerTime: 0, // Minutes to run
+    timerWorkTime: 25,
+    timerRestTime: 5,
   }),
   getters: {
     getTimerStatus: (state) => state.timerStatus,
@@ -16,6 +18,9 @@ export const useToolStore = defineStore("tool", {
   actions: {
     runTimer() {
       this.timerStatus = true
+      this.timerType 
+        ? this.timerTime = this.timerWorkTime 
+        : this.timerTime = this.timerRestTime
       this.timer = setInterval(() => {
         this.timerTime = this.timerTime - 1
         if (this.timerTime === 0) { // Timer done
@@ -28,6 +33,10 @@ export const useToolStore = defineStore("tool", {
     },
     invertTimerType() {
       this.timerType = !this.timerType
-    }
+    },
+    updateTimerSettings(workTime, restTime) {
+      this.timerWorkTime = workTime
+      this.timerRestTime = restTime
+    },
   },
 });

86dc2de5b590d7f5d50db6ce580259ca799687fa
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2022-09-13 17:16:53 -0300

    Add fields to note model

--- server/applications/notes/models.py
+++ server/applications/notes/models.py
@@ -1,11 +1,29 @@
 from django.db import models
 from django.urls import reverse
 from config.fields_default_values import (CHARFIELD_LONG)
+from django.contrib.auth.models import User
 
 # Create your models here.
 class NoteModel(models.Model):
 
-    title = models.CharField(max_length=CHARFIELD_LONG)
+    title = models.CharField(
+        max_length=CHARFIELD_LONG,
+        blank=True,
+        null=True,
+    )
+    parent = models.ForeignKey(
+        "self",
+        models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        blank=False,
+        null=False
+    )
+    content = models.TextField()
 
     class Meta:
         verbose_name = 'Note'

e631877618ef16444efca481b3633e7811358dd7
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2022-09-16 13:01:36 -0300

    Add retrieve user id in log in

--- client/src/stores/user-store.js
+++ client/src/stores/user-store.js
@@ -5,25 +5,39 @@ export const useUserStore = defineStore('user', {
   state: () => ({
     isLogged: false,
     userToken: null,
+    userId: null,
     username: "",
   }),
   getters: {
     isUserLogged: (state) => state.isLogged,
     getUsername: (state) => state.username,
+    getUserId: (state) => state.userId,
   },
   actions: {
     async logUser(username, password) {
-      const thisStore = useUserStore()
-      thisStore.saveUsername(username)
-      await api.auth.postUserAuthentication(username, password)
-        .then( result => {
+      const userStore = useUserStore()
+      userStore.saveUsername(username)
+      await api.user.postUserAuthentication(username, password)
+        .then( async result => {
           if (result.code === 200) {
+            await userStore.retrieveUserId()
             this.userToken = result.token
             this.isLogged = true
           } else {
             console.warn("Failed login")
           }
         }) 
+    },
+    async retrieveUserId() {
+      await api.user.getUserIdByUsername(this.getUsername)
+        .then( result => {
+          if (result.code === 200) {
+            this.userId = result.userId
+          } else {
+            console.warn("Failed to get user id")
+          }
+
+      })
     },
     saveUsername(username) {
       this.username = username

3a530d3788018c2011909181706325d8b58b941a
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2022-09-15 21:57:14 -0300

    Add preview of rendered note markdown

---
+++ client/src/components/MarkdownPreview.vue
@@ -0,0 +1,39 @@
+<template>
+  <div v-html="html"></div>
+</template>
+
+<script>
+import MarkdownIt from "markdown-it"
+import { ref, onBeforeMount, onBeforeUpdate } from "vue"
+
+export default {
+  props: ["md"],
+  setup(props) {
+    const markdownIt = new MarkdownIt()
+    const html = ref("")
+
+    function render() {
+      html.value = markdownIt.render(props.md)
+    }
+
+    onBeforeMount(() => {
+      render()
+    })
+    onBeforeUpdate(() => {
+      render()
+    })
+
+    return {
+      html,
+    }
+
+
+
+  }
+
+}
+</script>
+
+<style>
+
+</style>

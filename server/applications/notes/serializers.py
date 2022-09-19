3d68c0e7536132cbed501c144b9680541b06f60f
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2022-09-19 00:15:26 -0300

    Add validator for uniqueness identifier

--- server/applications/notes/serializers.py
+++ server/applications/notes/serializers.py
@@ -1,3 +1,5 @@
+from django.db.models import Q
+
 from rest_framework import serializers
 from applications.notes.models import NoteModel
 
@@ -7,3 +9,13 @@ class NoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = NoteModel
         fields = "__all__"
+
+    def validate_identifier(self, data):
+        query = Q(parent=self.initial_data["parent"]) & Q(identifier=self.initial_data["identifier"])
+        identifiers_notes_of_same_parent = NoteModel.objects.filter(query)
+        if identifiers_notes_of_same_parent.exists():
+            raise serializers.ValidationError(
+                self.initial_data["identifier"] + " is repeated"
+            )
+
+        return data

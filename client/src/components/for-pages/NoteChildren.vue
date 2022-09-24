079e9b7364c3793055a8500ff7043859357cbe99
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2022-09-24 20:03:27 -0300

    Fix link missing identifier in notes list

--- client/src/components/for-pages/NoteChildren.vue
+++ client/src/components/for-pages/NoteChildren.vue
@@ -1,6 +1,6 @@
 <template>
-  <router-link :to="{name: 'note', params: {identifier: props.note.id}}">
-    {{ props.note.title }} 
+  <router-link :to="{name: 'note', params: {identifier: identifier}}">
+    {{identifier}} - {{ props.note.title }} 
   </router-link>
   <ul>
     <li v-for="note in children" :key="note.id">
@@ -13,25 +13,30 @@
 import { ref, onBeforeMount } from "vue"
 
 import { useNoteStore } from "src/stores/note-store"
+import { createNoteIdentifier } from "src/utils/index"
 
 export default {
   props: ["note"],
   setup(props) {
     const children = ref([])
     const noteStore = useNoteStore()
+    const identifier = ref([])
 
-    function filterChildren() {
-      children.value = noteStore.getNotesByParent(props.note.id)
-    }
 
     onBeforeMount(() => {
-      filterChildren()      
+      children.value = noteStore.getNotesByParent(props.note.id)
+      identifier.value = createNoteIdentifier(
+        props.note,
+        noteStore.notesList,
+        [String(props.note.identifier),]
+      )
     })
 
 
     return {
       props,
       children,
+      identifier,
     }
     
   }

e45be110564d3e10609dc3897d312b9c8940c9be
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2022-09-29 19:40:17 -0300

    Add goals and objectives to admin site

--- server/applications/schedule/admin.py
+++ server/applications/schedule/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from applications.schedule.models import ObjectiveModel, GoalModel
+
 # Register your models here.
+admin.site.register(ObjectiveModel)
+admin.site.register(GoalModel)
+

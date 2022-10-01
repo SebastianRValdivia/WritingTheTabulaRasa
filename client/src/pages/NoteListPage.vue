e953511e0a2c8778dd3bf7228096a3d7c870cd94
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2022-10-01 09:26:18 -0300

    Add loading state to note list page

--- client/src/pages/NoteListPage.vue
+++ client/src/pages/NoteListPage.vue
@@ -1,57 +1,66 @@
 <template>
-  <div class="row">
-    <q-list id="note-list" class="column">
-      <q-item v-for="note in rootNotes" :key="note.id">
-        <NoteChildren :note="note" />
-      </q-item>
-    </q-list>
-    <q-separator vertical/>
-    <router-view ></router-view>
+  <div v-if="isLoading" class="window-height row justify-center items-center">
+    <q-spinner-rings
+      color="primary"
+      size="10rem"
+    />
   </div>
 
-
-  <br />
-  <q-separator />
-  <div class="row">
-    <div class="column">
-      <span class="text-h5">{{ $t("notePages.fleetingNotes") }}</span>
+  <div v-else>
+    <div class="row">
+      <q-list id="note-list" class="column">
+        <q-item v-for="note in rootNotes" :key="note.id">
+          <NoteChildren :note="note" />
+        </q-item>
+      </q-list>
+      <q-separator vertical/>
+      <router-view ></router-view>
     </div>
-  </div>
 
-  <div class="row">
-    <q-card 
-      class="column other-note-card" 
-      v-for="fleetingNote in noteStore.getFleetingNotes" 
-      :key="fleetingNote.id"
-    >
-      <q-card-section class="q-pt-none">
-        {{ fleetingNote.content }}
-      </q-card-section>
-    </q-card>
-    <div 
-      class="column other-note-card content-center q-pt-xl"
-      v-if="!isAddingFleetingNote"
-    >
-      <q-btn icon="add" round @click="toggleNewFleetingNote"/>
+
+    <br />
+    <q-separator />
+    <div class="row">
+      <div class="column">
+        <span class="text-h5">{{ $t("notePages.fleetingNotes") }}</span>
+      </div>
     </div>
-    <q-card 
-      class="column other-note-card" 
-      v-else
-    >
-      <q-card-section>
-        <q-input
-          v-model="newFleetingNoteContent"
-          type="textarea"
-          autogrow
-        />
-      </q-card-section>
-      <q-card-actions align="right" class="absolute-bottom">
-        <q-btn round icon="cancel" @click="toggleNewFleetingNote"/>
-        <q-btn round icon="check" @click="saveNewFleetingNote"/>
-      </q-card-actions>
-    </q-card>
-  </div>
 
+    <div class="row">
+      <q-card 
+        class="column other-note-card" 
+        v-for="fleetingNote in noteStore.getFleetingNotes" 
+        :key="fleetingNote.id"
+      >
+        <q-card-section class="q-pt-none">
+          {{ fleetingNote.content }}
+        </q-card-section>
+      </q-card>
+      <div 
+        class="column other-note-card content-center q-pt-xl"
+        v-if="!isAddingFleetingNote"
+      >
+        <q-btn icon="add" round @click="toggleNewFleetingNote"/>
+      </div>
+      <q-card 
+        class="column other-note-card" 
+        v-else
+      >
+        <q-card-section>
+          <q-input
+            v-model="newFleetingNoteContent"
+            type="textarea"
+            autogrow
+          />
+        </q-card-section>
+        <q-card-actions align="right" class="absolute-bottom">
+          <q-btn round icon="cancel" @click="toggleNewFleetingNote"/>
+          <q-btn round icon="check" @click="saveNewFleetingNote"/>
+        </q-card-actions>
+      </q-card>
+    </div>
+
+  </div>
 </template>
 
 <script>
@@ -66,6 +75,7 @@ export default {
     NoteChildren
   },
   setup() {
+    const isLoading = ref(false)
     const noteStore = useNoteStore()
     const userStore = useUserStore()
     const rootNotes = ref([])
@@ -92,6 +102,7 @@ export default {
     }
 
     onBeforeMount(async () => {
+      isLoading.value = true
       await noteStore.retrieveNotes()
       if (userStore.isUserLogged) {
         filterUserNotes()
@@ -99,10 +110,12 @@ export default {
         filterNotes()
       }
       await noteStore.retrieveFleetingNotes()
+      isLoading.value = false
     })
 
 
     return {
+      isLoading,
       noteStore,
       rootNotes,
       newFleetingNoteContent,

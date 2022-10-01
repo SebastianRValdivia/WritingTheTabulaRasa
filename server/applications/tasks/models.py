6e8a0c3b451720d82c447ad07866c71c8df09e6e
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2022-10-01 10:43:08 -0300

    Add owner field to task model

--- server/applications/tasks/models.py
+++ server/applications/tasks/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 from config.fields_default_values import (CHARFIELD_LONG)
 
@@ -12,6 +13,12 @@ class TaskModel(models.Model):
         on_delete=models.SET_NULL,
         blank=True,
         null=True,
+    )
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        blank=False,
+        null=False,
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)

59df0845a21eafe3aeb4062dd33ea23a96383af7
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2022-10-01 10:29:36 -0300

    Add tasks app with the task model

---
+++ server/applications/tasks/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.urls import reverse
+
+from config.fields_default_values import (CHARFIELD_LONG)
+
+# Create your models here.
+class TaskModel(models.Model):
+
+    title = models.CharField(max_length=CHARFIELD_LONG)
+    require = models.ForeignKey(
+        "self",
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    
+
+    class Meta:
+        verbose_name = "Task"
+        verbose_name_plural = "Tasks"
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("TaskModel_detail", kwargs={"pk": self.pk})
+
+

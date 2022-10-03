98c3c1932693cf4fcd817304b3fdd16fa54640e1
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2022-10-03 17:42:44 -0300

    Add preview of user task

--- client/src/pages/TaskListPage.vue
+++ client/src/pages/TaskListPage.vue
@@ -1,8 +1,9 @@
 <template>
   <LoadingSpinner v-if="isLoading" />
-  <div class="row">
+
+  <div v-else class="row">
     <q-list>
-      <q-item v-for="task in taskStore.getTaskList" :key="task.id">
+      <q-item v-for="task in displayedTasks" :key="task.id">
         <q-item-section>
           {{ task.title }}
         </q-item-section>
@@ -15,6 +16,7 @@
 import { ref, onBeforeMount } from "vue"
 
 import { useTaskStore } from "src/stores/task-store"
+import { useUserStore } from "src/stores/user-store"
 import LoadingSpinner from "src/components/LoadingSpinner"
 
 export default {
@@ -24,16 +26,19 @@ export default {
   setup() {
     const isLoading = ref(false)
     const taskStore = useTaskStore()
+    const userStore = useUserStore()
+    const displayedTasks = ref([])
 
     onBeforeMount(async () => {
       isLoading.value = true
       await taskStore.retrieveTasks()
+      displayedTasks.value = taskStore.getTaskByUser(userStore.getUserId)
       isLoading.value = false
     })
 
     return {
       isLoading,
-      taskStore,
+      displayedTasks,
     }
 
   }

efcd6928df91ef903b44031da738a32dcdefb278
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2022-10-06 17:34:45 -0300

    Add basic resource model

--- server/applications/resources/models.py
+++ server/applications/resources/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from config.fields_default_values import (CHARFIELD_LONG)
 
 # Create your models here.
+class ResourceModel(models.Model):
+
+    title = models.CharField(max_length=CHARFIELD_LONG)
+    observations = models.TextField()
+
+    class Meta:
+        verbose_name = "Resource"
+        verbose_name_plural = "Resources"
+
+    def __str__(self):
+        return self.title
+
+

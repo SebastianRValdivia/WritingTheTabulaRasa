bac63b5d686361a06dceeaf763439d37cf2d3c45
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2022-10-09 18:36:31 -0300

    Add input to add task and show it

--- client/src/pages/TaskListPage.vue
+++ client/src/pages/TaskListPage.vue
@@ -1,19 +1,23 @@
 <template>
   <LoadingSpinner v-if="isLoading" />
 
-  <div v-else class="row">
-    <q-list>
-      <q-item v-for="task in displayedTasks" :key="task.id">
+  <q-page v-else>
+    <div class="row q-pa-md q-gutter-sm">
+      <q-input v-model="newTask.title" class="col-6 "/>
+      <q-btn @click="addNewTask" icon="add" size="md"/>
+    </div>
+    <q-list class="row">
+      <q-item class="col-12" v-for="task in displayedTasks" :key="task.id">
         <q-item-section>
           {{ task.title }}
         </q-item-section>
       </q-item>
     </q-list>
-  </div>
+  </q-page>
 </template>
 
 <script>
-import { ref, onBeforeMount } from "vue"
+import { ref, reactive, computed, onBeforeMount } from "vue"
 
 import { useTaskStore } from "src/stores/task-store"
 import { useUserStore } from "src/stores/user-store"
@@ -22,23 +26,37 @@ import LoadingSpinner from "src/components/LoadingSpinner"
 export default {
   components: {
     LoadingSpinner,
-},
+  },
   setup() {
     const isLoading = ref(false)
     const taskStore = useTaskStore()
     const userStore = useUserStore()
-    const displayedTasks = ref([])
+
+    const newTask = reactive({
+      title: "",
+    })
+
+    const displayedTasks = computed(() => {
+      return taskStore.getTaskByUser(userStore.getUserId)
+    })
+
+
+    async function addNewTask() {
+      await taskStore.addNewTask(newTask)
+    }
 
     onBeforeMount(async () => {
       isLoading.value = true
       await taskStore.retrieveTasks()
-      displayedTasks.value = taskStore.getTaskByUser(userStore.getUserId)
       isLoading.value = false
     })
 
     return {
       isLoading,
       displayedTasks,
+      taskStore,
+      newTask,
+      addNewTask,
     }
 
   }

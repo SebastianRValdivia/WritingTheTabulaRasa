3d40e4cf8aa9b7fff4e07a996504e23e4f1fab62
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2022-10-13 07:55:49 -0300

    Add cheatsheet store

---
+++ client/src/stores/cheatsheet-store.js
@@ -0,0 +1,37 @@
+import { defineStore } from "pinia"
+import api from "src/api"
+
+export const useCheatsheetStore = defineStore("cheatsheet", {
+  state: () => ({
+    sheets: [],
+    cheats: []
+  }),
+  getters: {
+    getSheets: (state) => state.sheets,
+    getCheatsBySheet: (state) => {
+      return (sheetId) => state.cheats.filter((cheat) => cheat.sheet === sheetId)
+    }
+  },
+  actions: {
+    async retrieveCheats() {
+      let result = await api.cheatsheets.getCheats()
+
+      if (result.code === 200) {
+        this.cheats = result.cheats
+        return true
+      } else {
+        return false
+      }
+    },
+    async retrieveSheets() {
+      let result = await api.cheatsheets.getSheets()
+
+      if (result.code === 200) {
+        this.sheets = result.sheets
+        return true
+      } else {
+        return false
+      }
+    }
+  }
+})

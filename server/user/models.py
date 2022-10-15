0df1f375b4f9386fda81f62ecb5bd9a28da3d140
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2022-10-15 14:02:15 -0300

    Add fields to user preferences

--- server/user/models.py
+++ server/user/models.py
@@ -1,23 +1,34 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from config.fields_default_values import (CHARFIELD_SHORT)
+
 # Create your models here.
 
 class UserPreferences(models.Model):
 
-  user = models.ForeignKey(
-      User,  
-      on_delete=models.CASCADE
-  )
+    class ThemeChoices(models.TextChoices):
+        ## Themes Choice
+        DARK = "DARK", "Dark"
+        LIGHT = "LIGHT", "Light"
+
+    owner = models.OneToOneField( 
+        User,  
+        on_delete=models.CASCADE
+    )
+    lang = models.CharField(max_length=CHARFIELD_SHORT, default="en-US")
+    theme = models.CharField(
+        max_length=CHARFIELD_SHORT,
+        choices=ThemeChoices.choices,
+        default=ThemeChoices.LIGHT
+    )
+
   
     
-  class Meta:
-    verbose_name = "UserPreference"
-    verbose_name_plural = "UserPreferences"
+    class Meta:
+        verbose_name = "UserPreference"
+        verbose_name_plural = "UserPreferences"
 
-  def __str__(self):
-    return self.user.username
-
-  def get_absolute_url(self):
-    return reverse("UserPreferences_detail", kwargs={"pk": self.pk})
+    def __str__(self):
+        return self.owner.username
 

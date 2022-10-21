49f3df60aa1239ea4b8ac0c3d612c20eb8cfa877
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2022-10-21 16:26:43 -0300

    Add encyclopedia page api view set

--- server/applications/encyclopedia/views.py
+++ server/applications/encyclopedia/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+from applications.encyclopedia.models import EncyclopediaPageModel
+from applications.encyclopedia.serializers import EncyclopediaPageSerializer
 
 # Create your views here.
+
+class EncyclopediaPageViewSet(ModelViewSet):
+    
+    queryset = EncyclopediaPageModel.objects.all()
+    serializer_class = EncyclopediaPageSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+

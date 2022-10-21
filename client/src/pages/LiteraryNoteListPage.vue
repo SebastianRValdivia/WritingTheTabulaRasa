17d04b9fc66b55fac86672494f7df7ba2751bef2
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2022-10-21 14:41:21 -0300

    Add note tabs to LiteraryNoteListPage

--- client/src/pages/LiteraryNoteListPage.vue
+++ client/src/pages/LiteraryNoteListPage.vue
@@ -12,16 +12,25 @@
 <script>
 import { onBeforeMount } from "vue"
 import { useQuasar } from "quasar"
+import { useI18n } from "vue-i18n"
+import { useAppStore } from "src/stores/app-store"
 
 import { useNoteStore } from "src/stores/note-store"
 
 export default {
   setup() {
     const $q = useQuasar()
+    const appStore = useAppStore()
+    const { t } = useI18n()
     const noteStore = useNoteStore()
 
     onBeforeMount(async () => {
       $q.loading.show()
+      appStore.setTabs({
+        [t("notePages.permanent")]: "notes",
+        [t("notePages.fleeting")]: "fleetingNotes",
+        [t("notePages.literary")]: "literaryNotes",
+      })
       await noteStore.retrieveLiteraryNotes()
       $q.loading.hide()
     })

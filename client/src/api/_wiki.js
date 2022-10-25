e45163865bfc5fa1bc6baa77b8a6d75d1abbb948
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2022-10-25 16:59:22 -0300

    Add wiki api retrieve calls

---
+++ client/src/api/_wiki.js
@@ -0,0 +1,38 @@
+import { api } from "boot/axios"
+
+export default {
+  async getWikiPages(url=null, previous=[]) {
+    let response = url === null 
+    ? await api.get("v1/encyclopedia/pages/") 
+    : await api.get(url)
+
+    let data = [...previous, ...response.data.results]
+
+    if (response.status === 200 && response.data.next === null) {
+      return {
+        code: response.status,
+        pages: data
+      } 
+    } else if (response.data.next !== null) {
+        return this.getWikiPages(response.data.next, data)
+    } else {
+      return {
+        code: response.status,
+      }
+    }
+  },
+  async getWikiPageByUrl(url) {
+    let response = await api.get(`v1/encyclopedia/pages/?url=${url}`)
+
+    if (response.status === 200 && response.data.results[0] !== undefined) {
+      return {
+        code: 200,
+        page: response.data.results[0]
+      }
+    } else {
+      return {
+        code: 404
+      }
+    }
+  }
+}

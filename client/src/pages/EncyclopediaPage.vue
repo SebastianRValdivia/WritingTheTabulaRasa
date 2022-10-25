ad6f5c2d32ddd66967478cae0049a1dd5d5cafd7
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2022-10-25 16:59:42 -0300

    Add basic wiki page

---
+++ client/src/pages/EncyclopediaPage.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+  {{ props.title }}
+  </div>
+</template>
+
+<script>
+import { onBeforeMount } from "vue"
+import { useQuasar } from "quasar"
+import { useRouter } from "vue-router"
+
+import { useWikiStore } from "src/stores/wiki-store"
+
+export default {
+  props: {
+    title: String,
+  },
+  setup(props) {
+    const $q = useQuasar()
+    const $router = useRouter()
+    const wikiStore = useWikiStore()
+
+    onBeforeMount(async () => {
+      $q.loading.show()
+      let wikiFromStore = wikiStore.getWikiPageByUrl(props.title) // Search in the store
+      if (wikiFromStore === undefined) { // Not in the store, then try api and assign again
+        await wikiStore.retrieveWikiPageByUrl(props.title)
+        wikiFromStore = wikiStore.getWikiPageByUrl(props.title)
+        if (wikiFromStore === undefined) { // Doesn't exist
+          $router.push({name: "NotFound"})
+        }
+      }
+
+
+      $q.loading.hide()
+    })
+
+    return {
+      props
+
+    }
+  }
+}
+</script>

14638108f47b8aff4ad14a187b6b2b462e0e3edd
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2022-10-31 18:32:51 -0300

    Add basic card preview to wiki page

--- client/src/pages/EncyclopediaPage.vue
+++ client/src/pages/EncyclopediaPage.vue
@@ -1,9 +1,21 @@
 <template>
   <q-page v-if="pageData">
-    <h1>{{ pageData.title }}</h1>
-    <h4>{{ pageData.epigraph }}</h4>
+    <div class="row">
+      <h1 class="col-12">{{ pageData.title }}</h1>
+      <h4 class="col-10">{{ pageData.epigraph }}</h4>
+      <q-card v-if="pageCard" class="wiki-card">
+        <q-card-section>
+          <MarkdownPreview :md="pageCard.content" />
+        </q-card-section>
+      </q-card>
+    </div>
 
-    <MarkdownPreview :md="pageData.content" />
+    <q-separator inset />
+
+    <div class="row">
+      <MarkdownPreview :md="pageData.content" />
+    </div>
+
   </q-page>
 </template>
 
@@ -12,6 +24,8 @@ import { ref, onBeforeMount } from "vue"
 import { useQuasar } from "quasar"
 import { useRouter } from "vue-router"
 
+
+import api from "src/api"
 import { useWikiStore } from "src/stores/wiki-store"
 import MarkdownPreview from "src/components/MarkdownPreview"
 
@@ -28,11 +42,11 @@ export default {
     const wikiStore = useWikiStore()
 
     const pageData = ref()
+    const pageCard = ref()
 
     onBeforeMount(async () => {
       $q.loading.show()
       let wikiFromStore = wikiStore.getWikiPageByUrl(props.title) // Search in the store
-      console.log(wikiFromStore)
       if (wikiFromStore === undefined) { // Not in the store, then try api and assign again
         await wikiStore.retrieveWikiPageByUrl(props.title)
         wikiFromStore = wikiStore.getWikiPageByUrl(props.title)
@@ -44,14 +58,30 @@ export default {
       } else {
         pageData.value = wikiFromStore
       }
+
+      let cardFromServer = await api.wiki.getWikiCardByPageId(pageData.value.id)
+      pageCard.value = cardFromServer.card
       $q.loading.hide()
     })
 
     return {
       props,
       pageData,
-
+      pageCard,
     }
   }
 }
 </script>
+
+<style>
+.wiki-card {
+  min-width: 350px;
+  max-width: 400px;
+}
+.wiki-card h1 {
+  border: 1px solid black;
+  font-size: 1.5rem;
+  line-height: 1;
+  text-align: center;
+}
+</style>

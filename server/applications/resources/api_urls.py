66daeb3364ffb224a78339e957a5074458828039
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2022-11-29 19:43:26 -0300

    Add image api

--- server/applications/resources/api_urls.py
+++ server/applications/resources/api_urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from applications.resources.views import ReferenceViewSet
+from applications.resources.views import ReferenceViewSet, ImageViewSet
 
 router = DefaultRouter()
 router.register("reference", ReferenceViewSet, basename="references")
+router.register("images", ImageViewSet, basename="images")
 
 urlpatterns = [
   path("", include(router.urls))
--- server/applications/resources/serializers.py
+++ server/applications/resources/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework.serializers import ModelSerializer
 
-from applications.resources.models import ReferenceModel
+from applications.resources.models import ReferenceModel, ImageModel
 
 class ReferenceSerializer(ModelSerializer):
 
     class Meta:
         model = ReferenceModel
+        fields = "__all__"
+
+class ImageSerializer(ModelSerializer):
+
+    class Meta:
+        model = ImageModel
         fields = "__all__"

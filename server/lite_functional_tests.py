3cf8cb0693115dab0db1442f844ab6a71f69c296
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2022-12-03 18:56:49 -0300

    Test for functionalities of lite version

---
+++ server/lite_functional_tests.py
@@ -0,0 +1,35 @@
+import os
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class FirstUserExperience(unittest.TestCase):
+
+    def setUp(self):
+        """ Before each test do: """
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        """ After each test do: """
+        self.browser.quit()
+
+    def test_home_title_is_tabula_rasa(self):
+        """ User enters the website and sees tabula rasa in the title """
+        url = "http://localhost:8000/lite/"
+        self.browser.get(url=url)
+
+        self.assertIn('Tabula Rasa', self.browser.title)
+    
+    def test_navbar_contains_link_to_home(self):
+        """ User sees the navigation bar and click on index to go to index page"""
+        url = "http://localhost:8000/lite/"
+        self.browser.get(url=url)
+        self.browser.find_element(By.LINK_TEXT, "Index").click()
+
+        self.assertEqual(self.browser.current_url, url)
+
+
+
+if __name__ == '__main__':
+    unittest.main()

541bddbf4eeffd129754113bc227a0695f58f2dc
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2022-12-04 06:49:50 -0300

    Test for lite version index page

--- server/lite/tests.py
+++ server/lite/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.urls import path, include, reverse
 
 # Create your tests here.
+class LiteTests(TestCase):
+    urlpatterns = [
+        path("lite/", include("lite.urls"))
+    ]
+
+    def test_url_to_lite_shows_index_page(self):
+        url = reverse("index")
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, "lite/index.html")
+
+

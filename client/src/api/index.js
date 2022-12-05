846de94912112d4304de740d9c6ecc74261d2ba7
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2022-12-05 11:06:10 -0300

    Add post image to global api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,12 +4,14 @@ import schedule from "./_schedule"
 import tasks from "./_tasks"
 import cheatsheets from "./_cheatsheets"
 import wiki from "./_wiki"
+import resources from "./_resources.js"
 
 export default {
-    user,
-    notes,
-    schedule,
-    tasks,
-    cheatsheets,
-    wiki,
+  user,
+  notes,
+  schedule,
+  tasks,
+  cheatsheets,
+  wiki,
+  resources,
 }

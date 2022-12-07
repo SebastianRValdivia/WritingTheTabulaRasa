7046438a70c4fdacb2ed76b1899b552719552d68
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2022-12-07 12:34:16 -0300

    Add lite log in page

--- server/lite/urls.py
+++ server/lite/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from lite.views import IndexPageView
 
 urlpatterns = [
-    path("", IndexPageView.as_view(), name="index")
+    path("", IndexPageView.as_view(), name="index"),
+    path("user/", include("user.urls"))
 ]

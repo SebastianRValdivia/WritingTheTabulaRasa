ccf2706fe3c9f9686984f853f4dc3b2c1cff5a9d
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2022-12-08 09:06:40 -0300

    Add research page and it's path in router

---
+++ client/src/pages/ResearchPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page>
+    {{ props.id }}
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "ResearchPage",
+  props: {
+    id: String
+  },
+  setup(props) {
+
+    return {
+      props
+    }
+  }
+}
+</script>

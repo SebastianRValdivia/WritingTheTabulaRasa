df5eb56ae151ac5af5e2b406245945075a6a7304
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2022-12-09 11:12:09 -0300

    Add intersection and cards to sheet page

--- client/src/pages/CheatsheetListPage.vue
+++ client/src/pages/CheatsheetListPage.vue
@@ -1,19 +1,39 @@
 <template>
-  <ul v-if="cheatsheetStore.getSheets.length > 0">
-    <li v-for="sheet in cheatsheetStore.getSheets" :key="sheet.id">
-      <router-link :to="{name: 'cheatsheet', params: {title: sheet.url}}">
-        {{ sheet.title }}
-      </router-link>
-    </li>
-  </ul>
-
-  <div v-else class="row justify-center">
-    <h1>{{ $t("sheetPage.noSheets") }}</h1>
-  </div>
-
-  <q-page-sticky position="bottom-right" :offset="[18, 18]">
-    <q-btn round color="secondary" icon="add" size="md" :to="{name: 'newCheatsheet' }"/>
-  </q-page-sticky>
+  <q-page>
+    <div v-if="cheatsheetStore.getSheets.length > 0" class="row justify-center q-gutter-md">
+      <q-itersection
+        v-for="sheet in cheatsheetStore.getSheets" 
+        :key="sheet.id" 
+      >
+        <q-card class="q-pa-md sheet-card">
+          <router-link :to="{name: 'cheatsheet', params: {title: sheet.url}}">
+            <q-card-section class="column items-center text-h4">
+              {{ sheet.title }}
+            </q-card-section>
+        
+            <q-card-section>
+              {{ sheet.description }}
+            </q-card-section>
+        
+          </router-link>
+        </q-card>
+      </q-itersection>
+    </div>
+    
+    <div v-else class="row justify-center">
+      <h1>{{ $t("sheetPage.noSheets") }}</h1>
+    </div>
+    
+    <q-page-sticky position="bottom-right" :offset="[18, 18]">
+      <q-btn 
+        round 
+        color="secondary" 
+        icon="add" 
+        size="md" 
+        :to="{name: 'newCheatsheet' }"
+      />
+    </q-page-sticky>
+  </q-page>
 </template>
 
 <script>
@@ -46,3 +66,10 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.sheet-card {
+  min-width: 20rem;
+  min-height: 20rem;
+}
+</style>

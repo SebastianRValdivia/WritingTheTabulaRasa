2d7e79b463a3bd23dd57cf2c43f81a6bfe776a3e
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2022-12-09 14:53:18 -0300

    Add fuzzy search to cheatsheet

--- client/src/pages/CheatsheetListPage.vue
+++ client/src/pages/CheatsheetListPage.vue
@@ -4,8 +4,19 @@
       v-if="cheatsheetStore.getSheets.length > 0" 
       class="row justify-center q-gutter-lg"
     >
+      <div class="col-12">
+        <q-input 
+          rounded
+          outlined
+          v-model="searchInput"
+        >
+          <template v-slot:append>
+            <q-icon name="search" />
+          </template>
+        </q-input>
+      </div>
       <q-intersection
-        v-for="sheet in cheatsheetStore.getSheets" 
+        v-for="sheet in displayedSheets" 
         :key="sheet.id" 
         once
         transition="scale"
@@ -44,9 +55,10 @@
 </template>
 
 <script>
-import { onBeforeMount } from "vue"
+import { ref, computed, onBeforeMount } from "vue"
 import { useQuasar, useMeta } from "quasar"
 import { useI18n } from "vue-i18n"
+import Fuse from "fuse.js"
 
 import { useCheatsheetStore } from "src/stores/cheatsheet-store"
 
@@ -55,9 +67,32 @@ export default {
   setup() {
     const $q = useQuasar()
     const { t } = useI18n()
-
     const cheatsheetStore = useCheatsheetStore()
-    
+
+    const searchInput = ref("")
+    const displayedSheets = computed(() => {
+      if (searchInput.value) {
+        let i = searchSheet(searchInput.value)
+        console.log(i)
+        return i
+      } else return cheatsheetStore.getSheets
+    })
+
+    function searchSheet(searchPattern) {
+      let fuzzySearch = new Fuse(
+        cheatsheetStore.getSheets,
+        {
+          keys: [
+            "title",
+            "description",
+          ]
+        }
+      )
+      return fuzzySearch
+        .search(searchPattern)
+        .map((item) => item.item)
+    }
+
     onBeforeMount(async () => {
       $q.loading.show()
       await cheatsheetStore.retrieveSheets()
@@ -69,6 +104,8 @@ export default {
     })
     return {
       cheatsheetStore,
+      displayedSheets,
+      searchInput,
     }
   }
 }

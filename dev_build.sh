3e5ac5acd8203e346a96196a2e119e7ea14816b3
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2022-12-15 17:46:27 -0300

    Add build script to run functional tests

---
+++ dev_build.sh
@@ -0,0 +1,17 @@
+# Build and deploy client into django to run tests
+# Requires npm, quasar cli, python3 and pip3 installed
+
+cd ./client/ # Goes to client/ to build spa
+npm install
+quasar build
+cd ../ # Back to root and moves files to server
+mv -v ./client/dist/spa/index.html ./server/webapp/templates/webapp/
+mv -v ./client/dist/spa/css ./server/webapp/staticfiles/
+mv -v ./client/dist/spa/fonts ./server/webapp/staticfiles/
+mv -v ./client/dist/spa/icons ./server/webapp/staticfiles/
+mv -v ./client/dist/spa/img ./server/webapp/staticfiles/
+mv -v ./client/dist/spa/js ./server/webapp/staticfiles/
+mv -v ./client/dist/spa/favicon.ico ./server/webapp/staticfiles/
+cd ./server/ # Goes to server and install dependencies
+pip3 install -r requirements.txt
+python3 manage.py runserver & # Run server in localhost on debug mode

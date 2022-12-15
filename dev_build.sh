5d82c54fa2f72920830b9670b07de611ab690bd8
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2022-12-15 19:13:59 -0300

    Add createsuperuser to dev_build

--- dev_build.sh
+++ dev_build.sh
@@ -1,5 +1,6 @@
 # Build and deploy client into django to run tests
 # Requires npm, quasar cli, python3 and pip3 installed
+DJANGO_SUPERUSER_PASSWORD=password
 
 cd ./client/ # Goes to client/ to build spa
 npm install
@@ -14,4 +15,5 @@ mv -v ./client/dist/spa/js ./server/webapp/staticfiles/
 mv -v ./client/dist/spa/favicon.ico ./server/webapp/staticfiles/
 cd ./server/ # Goes to server and install dependencies
 pip3 install -r requirements.txt
-python3 manage.py runserver & # Run server in localhost on debug mode
+python3 ./manage.py createsuperuser --no-input --username admin --email admin@mail.com # Create admin user
+python3 ./manage.py runserver & # Run server in localhost on debug mode

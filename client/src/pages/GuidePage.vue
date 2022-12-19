e35c708c631168e8aa21663d5bb9fd2c5ebbcd53
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2022-12-19 19:44:40 -0300

    Add raw guide page

--- client/src/pages/GuidePage.vue
+++ client/src/pages/GuidePage.vue
@@ -1,10 +1,22 @@
 <template>
-  <q-page>
-    {{ props.url }}
+  <q-page class="q-pa-md">
+    <h2 class="text-h2">{{guideData.title}}</h2>
+    <p class="text-subtitle1">
+      {{ guideData.description }}
+    </p>
+    <ol>
+      <li
+      v-for="stepData in stepsDataList"
+      :key="stepData.id"
+      >{{ stepData.title }}</li>
+    </ol>
   </q-page>
 </template>
 
 <script>
+import { ref, onBeforeMount } from "vue"
+
+import api from "src/api"
 
 export default {
   name: "GuidePage",
@@ -12,9 +24,29 @@ export default {
     url: String
   },
   setup(props) {
+    
+    const guideData = ref({})
+    const stepsDataList = ref([])
 
+    async function loadPage(pageUrl) {
+      // Retrieve page
+      let result = await api.guides.getGuideByUrl(pageUrl)
+      guideData.value = result.guide
+
+      if (result) {
+        result = await api.guides.getStepsByGuideId(guideData.value.id)
+        stepsDataList.value = result.steps
+      }
+    }
+
+    onBeforeMount(async () => {
+      let result = await loadPage(props.url)
+
+    })
     return {
-      props
+      props,
+      guideData,
+      stepsDataList,
     }
   }
 }

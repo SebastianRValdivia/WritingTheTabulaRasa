7191c5b00f04786246abeb2bc6b58335bb9b521d
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2022-12-23 19:47:13 -0300

    Add internationalization to lite

--- server/config/settings.py
+++ server/config/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import environ
 import os
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / "subdir".
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,6 +76,7 @@ MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -145,6 +147,14 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+LANGUAGES =[ 
+    ('en', _('English')),
+]
+
+LOCALE_PATHS = (
+    BASE_DIR / "locale/",
+)
 
 
 # Static files (CSS, JavaScript, Images)

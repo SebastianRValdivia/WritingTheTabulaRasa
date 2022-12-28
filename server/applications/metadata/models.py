39829d8fd32309e5cf711ec39d3e43f292b795dc
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2022-12-28 20:10:35 -0300

    Add category model for metadata

--- server/applications/metadata/models.py
+++ server/applications/metadata/models.py
@@ -1,6 +1,22 @@
 from django.db import models
+from config.fields_default_values import CHARFIELD_LONG
 
 # Create your models here.
+class CategoryModel(models.Model):
+
+    name = models.CharField(
+        max_length=CHARFIELD_LONG,
+        blank=False,
+        null=False,
+    )
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
 class MetadataModel(models.Model):
 
     content = models.JSONField(

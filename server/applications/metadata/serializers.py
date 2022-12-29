16b2f88dd1d5003a74c2d83ce5b183ecdab2e3c3
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2022-12-29 17:09:33 -0300

    Add category rest api

--- server/applications/metadata/serializers.py
+++ server/applications/metadata/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework.serializers import ModelSerializer
 
-from applications.metadata.models import MetadataModel
+from applications.metadata.models import MetadataModel, CategoryModel
 
 class MetadataSerializer(ModelSerializer):
 
     class Meta:
         model = MetadataModel
+        fields = "__all__"
+
+class CategorySerializer(ModelSerializer):
+
+    class Meta:
+        model = CategoryModel
         fields = "__all__"

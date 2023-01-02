588e79c288cee5f108bf5cc6af7b3900409731fe
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2023-01-02 17:38:04 -0300

    Add fleeting note card independent component

---
+++ client/src/components/for-input/FleetingNoteCard.vue
@@ -0,0 +1,44 @@
+<template>
+  <q-card 
+    class="column fleeting-note-card" 
+  >
+    <q-card-actions align="right">
+      <q-btn 
+        icon="removeNote" 
+        round 
+        flat 
+        color="negative"
+        @click="removeNote"
+      />
+    </q-card-actions>
+    <q-card-section class="q-pt-xs">
+      {{ props.fleetingNoteData.content }}
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+import { useNoteStore } from "src/stores/note-store"
+
+export default {
+  name: "FleetingNoteCard",
+  props: {
+    fleetingNoteData: {
+      requiered: true
+    }
+  },
+
+  setup(props) {
+    const noteStore = useNoteStore()
+
+    async function removeNote() {
+      let result = await noteStore.removeFleetingNote(props.fleetingNoteData.id)
+    }
+    return {
+      props,
+      removeNote,
+    }
+
+  }
+}
+</script>

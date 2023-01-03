cb608db14adb78dec4c2f426054434477d99dfcc
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2023-01-03 13:09:47 -0300

    Add quizz and quizz questions to admin site

--- server/applications/quizzes/admin.py
+++ server/applications/quizzes/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from applications.quizzes.models import (
+    QuizzModel,
+    QuizzQuestionModel
+)
+
 # Register your models here.
+admin.site.register(QuizzModel)
+admin.site.register(QuizzQuestionModel)

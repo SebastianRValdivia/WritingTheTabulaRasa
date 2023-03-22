6baf5dc79161ca33ff24358a21f9eb45589467cd
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2023-03-22 06:29:03 -0300

    Add input of a flash card deck

--- client/src/pages/FlashCardCollectionEditorPage.vue
+++ client/src/pages/FlashCardCollectionEditorPage.vue
@@ -1,5 +1,111 @@
 <template>
-  <q-page padding>
-    <q-input />
+  <q-page padding class="row">
+    <q-input 
+      :label="$t('flashCardCollectionEditorPage.collectionTitle')"
+      class="col col-12"
+    />
+
+    <div class="col col-12 column items-center">
+      <q-card 
+        class="col-6 scoped-flashcard animated"
+        :class="{ flipInY: cardOnHintSide, flipInX: !cardOnHintSide }"
+      >
+        <q-card-section>
+          <q-input
+            v-if="cardOnHintSide"
+            class="q-mt-xl"
+            type="textarea"
+            autogrow
+            v-model="hintInput"
+            :label="$t('flashCardCollectionEditorPage.aHint')"
+          />
+          <q-input
+            v-else
+            class="q-mt-xl"
+            type="textarea"
+            autogrow
+            v-model="responseInput"
+            :label="$t('flashCardCollectionEditorPage.correctResponse')"
+          />
+        </q-card-section>
+        <q-card-actions class="column items-center absolute-bottom"> 
+          <q-btn 
+            v-if="cardOnHintSide"
+            icon="chevron_right"
+            @click="toggleToResponseSide"
+          />
+          <q-btn 
+            v-else
+            icon="chevron_left"
+            @click="toggleToHintSide"
+          />
+        </q-card-actions>
+      </q-card>
+      <q-btn 
+        icon="done"
+        @click="saveFlashCard"
+        class="q-mt-md q-mb-md"
+      />
+    </div>
+
+    <div class="row justify-center q-gutter-md">
+      <q-card
+        v-for="(flashCard, index) in flashCardsList"
+        :key="index"
+        class="scoped-flashcard col"
+      >
+        <q-card-section>
+          {{ flashCard.hint }}
+        </q-card-section>
+      </q-card>
+    </div>
+
   </q-page>
 </template>
+
+<script>
+import { ref } from "vue"
+
+export default {
+  name: "flashCardCollectionEditorPage",
+  setup() {
+    const flashCardsList = ref([])
+    const hintInput = ref("")
+    const responseInput = ref("")
+    const cardOnHintSide = ref(true)
+
+    function saveFlashCard() {
+      let newFlashCardData = {
+        hint: hintInput.value,
+        response: responseInput.value,
+      }
+      flashCardsList.value.push(newFlashCardData)
+    }
+    function toggleToResponseSide() {
+      cardOnHintSide.value = false
+    }
+    function toggleToHintSide() {
+      cardOnHintSide.value = true
+    }
+
+    return {
+      flashCardsList,
+      hintInput,
+      responseInput,
+      cardOnHintSide,
+
+      saveFlashCard,
+      toggleToResponseSide,
+      toggleToHintSide,
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+.scoped-flashcard {
+  min-height: 20rem;
+  min-width: 30rem;
+}
+</style>

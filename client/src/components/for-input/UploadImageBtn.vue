e67a513b0c0ba54bb066a5c2b65d5cf5c2ac4d95
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2023-04-22 18:41:59 -0300

    Add image upload btn to tool drawer

---
+++ client/src/components/for-input/UploadImageBtn.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-btn
+    icon="add_photo_alternate"
+    flat 
+    @click="openImageDialog"
+  >
+  </q-btn>
+</template>
+
+<script>
+import { useQuasar } from "quasar"
+
+import ImageUploadDialog from "src/components/for-control/ImageUploadDialog"
+
+export default {
+  name: "UploadImageBtn",
+  setup() {
+    const quasar = useQuasar()
+
+    function openImageDialog() {
+
+      quasar.dialog({
+        component: ImageUploadDialog
+      })
+
+    }
+
+    return {
+      openImageDialog
+    }
+  }
+}
+</script>
+

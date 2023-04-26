616168a7843bdc7bb63050b3fb2397a029ed24ac
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2023-04-26 15:47:26 -0300

    Add retrieval and raw display of learning res

--- client/src/pages/LearningResourceListPage.vue
+++ client/src/pages/LearningResourceListPage.vue
@@ -1,11 +1,41 @@
 <template>
   <q-page padding>
+
+    {{ displayedLearningResources }}
+    
   </q-page>
 </template>
 
 <script>
+import { ref, computed, onBeforeMount } from "vue"
+import { useQuasar } from "quasar"
+
+import { useResourceStore } from "src/stores/resource-store"
 
 export default {
   name: "LearningResourceListPage",
+  setup() {
+    const resourceStore = useResourceStore()
+    const quasar = useQuasar()
+
+    const displayedLearningResources = computed(() => {
+      return resourceStore.getLearningResources
+    })
+
+    onBeforeMount(async () => {
+      quasar.loading.show()
+      
+      let result = await resourceStore.retrieveLearningResources()
+
+      if (result) {
+        quasar.loading.hide()
+      }
+
+    })
+
+    return {
+      displayedLearningResources,
+    }
+  }
 }
 </script>

e3032a4fdd035fb9793d835cbd30f860bdf60cbc
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2023-05-11 17:21:52 -0300

    Test if all user data is passed from the api

--- server/user/tests.py
+++ server/user/tests.py
@@ -13,8 +13,16 @@ class UserTests(APITestCase, URLPatternsTestCase):
     User.objects.create_user("username", "mail@mail.com") # Create user
     url = reverse("user-data") + "?username=username"
     request = self.client.get(url) # Get user data
+    expected_result = {
+        'first_name': '',
+        'is_staff': False,
+        'is_superuser': False,
+        'last_name': '',
+        'pk': 1,
+        'username': 'username'
+    }
     self.assertEqual(request.status_code, 200)
-    self.assertEqual(dict(request.data["results"][0]), {"username": "username", "pk": 1}) # Check if first response is correct
+    self.assertEqual(dict(request.data["results"][0]), expected_result) # Check if first response is correct
 
   def test_retrieve_user_token(self):
     User.objects.create_user(username="username", password="password") # Create user

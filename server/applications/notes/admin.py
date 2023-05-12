ce48dc1cf0cf211f4bcf09e47ae13abe06561965
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2023-05-12 18:54:01 -0300

    Add note connections model to admin

--- server/applications/notes/admin.py
+++ server/applications/notes/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 
 from applications.notes.models import (
-  NoteModel, 
+  NoteModel,
+  NoteConnectionGroupModel,
   FleetingNoteModel,
   LiteraryNoteModel,
 )
 
 # Register your models here.
 admin.site.register(NoteModel)
+admin.site.register(NoteConnectionGroupModel)
 admin.site.register(FleetingNoteModel)
 admin.site.register(LiteraryNoteModel)
 

40eb690743f627d84c30890665945a134e959035
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2023-05-24 12:28:02 -0300

    Add permissions to tasks

--- server/applications/tasks/views.py
+++ server/applications/tasks/views.py
@@ -1,13 +1,18 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAdminUser
 
 from applications.tasks.serializers import TaskSerializer
 from applications.tasks.models import TaskModel
+from config.permissions import (IsOwner)
 
 # Create your views here.
 class TaskViewSet(ModelViewSet):
 
-    queryset = TaskModel.objects.all()
     serializer_class = TaskSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsOwner,]
     filterset_fields = ["owner"]
+
+    def get_queryset(self):
+        user = self.request.user
+        return TaskModel.objects.filter(owner=user.pk)
+

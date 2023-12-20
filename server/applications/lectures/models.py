4b9a5f7073d5f28eca48f31d05b23f885c56a610
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2023-12-20 11:31:01 -0300

    Add lecture model

--- server/applications/lectures/models.py
+++ server/applications/lectures/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import FileExtensionValidator
+from config.fields_default_values import (CHARFIELD_LONG)
 
 # Create your models here.
+
+
+class LectureModel(models.Model):
+
+
+    title = models.CharField(
+        max_length=CHARFIELD_LONG,
+        blank=False,
+        null=False,
+    )
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(
+        blank=False,
+        null=False,
+    )
+    video = models.FileField(
+        upload_to="lectures_videos",
+        null=True,
+        blank=True,
+        validators=[FileExtensionValidator(allowed_extensions=['mp4',])]
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Lecture"
+        verbose_name_plural = "Lectures"
+
+    def __str__(self):
+        return self.title

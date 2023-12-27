e5a0ad78f6ab5cc3ddd620dadb5f4d3ab5e31aec
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2023-12-27 15:09:35 -0300

    Add lecture card

---
+++ client/src/components/for-viewing/LectureCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-card class="my-card">
+  <q-img :src="props.thumbnail">
+    <div class="absolute-bottom text-h6">
+        {{ props.title }}
+    </div>
+  </q-img>
+</q-card>
+</template>
+
+<script>
+
+export default {
+  name: "LectureCard",
+  props: {
+    title: String,
+    description: String,
+    thumbnail: String,
+  },
+  setup(props) {
+    return {
+      props
+    }
+  }
+}
+
+</script>
